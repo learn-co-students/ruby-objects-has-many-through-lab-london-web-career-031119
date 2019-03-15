1e374744f58451ad6ee8f873ce9add6abef3f2d6
keoki33 <gwong323@yahoo.com>
2019-03-15 15:28:50 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+require 'pry'
+
+class Song
+attr_accessor :name, :artist, :genre
+attr_writer
+attr_reader
+@@all = []
+
+def self.all
+@@all
+end
+
+def initialize(name, artist, genre)
+@name = name
+@artist = artist
+@genre = genre
+@@all << self
+end
+
+end
